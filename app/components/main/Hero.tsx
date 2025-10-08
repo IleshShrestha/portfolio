5085550a1e79166293672006574a683e4d4ec0c2
eversitc <support@everestfcu.org>
2025-10-08 18:20:59 -0400

    added some stuff

---
+++ app/components/main/Hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState, useEffect } from "react";
+import HeroContent from "../sub/HeroContent";
+export default function Hero() {
+  const [videoOpacity, setVideoOpacity] = useState("opacity-100");
+
+  useEffect(() => {
+    // Start the transition to 50% opacity after a short delay
+    const timer = setTimeout(() => {
+      setVideoOpacity("opacity-[0.3]");
+    }, 1000); // 500ms delay before starting the fade
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className={`w-full h-full object-cover transition-opacity duration-[1500ms] ease-in-out ${videoOpacity}`}
+      >
+        <source src="/heroBackground.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full">
+        <HeroContent />
+      </div>
+    </div>
+  );
+}
