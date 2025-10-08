7c7101c6344c4c6c8e47b6d8b9daccb929e087a8
IleshShrestha <ilesh.k.shrestha@gmail.com>
2025-10-08 18:29:46 -0400

    change

--- app/components/main/Hero.tsx
+++ app/components/main/Hero.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import HeroContent from "../sub/HeroContent";
 export default function Hero() {
   const [videoOpacity, setVideoOpacity] = useState("opacity-100");
+  const [shrunk, setShrunk] = useState(false);
 
   useEffect(() => {
     // Start the transition to 50% opacity after a short delay
